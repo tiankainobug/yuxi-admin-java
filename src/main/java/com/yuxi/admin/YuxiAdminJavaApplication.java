bd6030f15f888e5f68ae04a400a081c53869617b
tiankai <tiankai@chinatelecom.cn>
2025-12-13 23:39:04 +0800

    feat: 初始化代码

---
+++ src/main/java/com/yuxi/admin/YuxiAdminJavaApplication.java
@@ -0,0 +1,13 @@
+package com.yuxi.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuxiAdminJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuxiAdminJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuxi/admin/YuxiAdminJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuxi.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuxiAdminJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
